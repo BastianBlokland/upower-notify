806ac46d6eb4ea3cfdb8ef0564ef4ac9f7b64921
Bastian Blokland <mail@bastian.tech>
2026-03-14 14:24:02 +0200

    Minor style fixes

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 	powerProfileDaemon, err := ppd.New()
 	var profileState ppd.State
 	if powerProfileDaemon != nil {
-		profileState, err = powerProfileDaemon.Get();
+		profileState, err = powerProfileDaemon.Get()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -94,7 +94,7 @@ func main() {
 
 			stateChanged := update.State != old.State
 			profileChanged := profileState.ActiveProfile != oldProfileState.ActiveProfile
-			percentageChanged := (uint32(update.Percentage)/notifyStep) != (uint32(lastNotifyPercentage)/notifyStep);
+			percentageChanged := (uint32(update.Percentage)/notifyStep) != (uint32(lastNotifyPercentage)/notifyStep)
 
 			if stateChanged || profileChanged || percentageChanged {
 				notifyState(update, profileState, notifier)
@@ -117,7 +117,7 @@ func formatDuration(d time.Duration) string {
 		}
 	}
 	if minutes > 0 {
-		if len(result) > 0 {
+		if result != "" {
 			result += " and "
 		}
 		result += fmt.Sprintf("%d minute", minutes)
@@ -130,40 +130,36 @@ func formatDuration(d time.Duration) string {
 
 func notifyState(battery upower.Update, profileState ppd.State, notifier *notify.Notifier) {
 	var msg string
-	var invalidState bool = false
+	var invalidState bool
 	switch battery.State {
 	case upower.Charging, upower.FullCharged, upower.PendingCharge, upower.PendingDischarge:
 		msg = fmt.Sprintf("%.0f%% %s", battery.Percentage, battery.State)
 		var strTillFull = formatDuration(battery.TimeToFull)
-		if len(strTillFull) != 0 {
+		if strTillFull != "" {
 			msg += fmt.Sprintf("\n%s until full", strTillFull)
 		}
 		if battery.EnergyRate != 0.0 {
 			msg += fmt.Sprintf("\n%.1f W usage", battery.EnergyRate)
 		}
-		break
 	case upower.Empty:
 		msg = fmt.Sprintf("%.0f%%", battery.Percentage)
 		if battery.EnergyRate != 0.0 {
 			msg += fmt.Sprintf("\n%.1f W usage", battery.EnergyRate)
 		}
-		break
 	case upower.Discharging:
 		msg = fmt.Sprintf("%.0f%% %s", battery.Percentage, battery.State)
 		var strTillEmpty = formatDuration(battery.TimeToEmpty)
-		if len(strTillEmpty) != 0 {
+		if strTillEmpty != "" {
 			msg += fmt.Sprintf("\n%s until empty", strTillEmpty)
 		}
 		if battery.EnergyRate != 0.0 {
 			msg += fmt.Sprintf("\n%.1f W usage", battery.EnergyRate)
 		}
-		break
 	default:
 		msg = fmt.Sprintf("%.0f%% Invalid State", battery.Percentage)
 		invalidState = true
-		break
 	}
-	if len(profileState.ActiveProfile) > 0 {
+	if profileState.ActiveProfile != "" {
 		msg += fmt.Sprintf(" (%s profile)", profileState.ActiveProfile)
 	}
 	if invalidState || battery.Percentage < 10 {
