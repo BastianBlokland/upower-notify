029d14f7c8855212cbe32f69d1ba4d1afd2c30c9
Bastian Blokland <mail@bastian.tech>
2026-08-10 22:14:10 +0200

    nix: Support ARM

--- flake.nix
+++ flake.nix
@@ -6,44 +6,49 @@
   };
 
   outputs =
-    { self, nixpkgs, ... }:
+    { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
       tmpDir = "/tmp/upower-notify";
     in
     {
-      packages.${system}.default = pkgs.buildGoModule {
-        pname = "upower-notify";
-        version = "0.1.0";
-        src = ./.;
-        vendorHash = "sha256-oPiRr9x0wEbEjwrB8KsO1vHCSV+f/dn4VTUI3NI4UpE=";
-        proxyVendor = true;
+      packages = forAllSystems (pkgs: {
+        default = pkgs.buildGoModule {
+          pname = "upower-notify";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = "sha256-oPiRr9x0wEbEjwrB8KsO1vHCSV+f/dn4VTUI3NI4UpE=";
+          proxyVendor = true;
 
-        meta = {
-          description = "Fork of: https://github.com/omeid/upower-notify for personal use.";
-          mainProgram = "upower-notify";
+          meta = {
+            description = "Fork of: https://github.com/omeid/upower-notify for personal use.";
+            mainProgram = "upower-notify";
+          };
         };
-      };
+      });
 
-      defaultPackage.${system} = self.packages.${system};
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nixfmt
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.nixfmt
+            pkgs.go
+            pkgs.gopls
+            pkgs.go-tools
+            pkgs.delve
+          ];
 
-          pkgs.go
-          pkgs.gopls
-          pkgs.go-tools
-          pkgs.delve
-        ];
-
-        # Avoid polluting our home directory.
-        GOPATH = "${tmpDir}/go";
-        GOENV = "${tmpDir}/go/env";
-        GOCACHE ="${tmpDir}/go/cache";
-        GOMODCACHE = "${tmpDir}/go/pkg/mod";
-        GOTELEMETRYDIR = "${tmpDir}/go/telemetry";
-      };
+          # Avoid polluting our home directory.
+          GOPATH = "${tmpDir}/go";
+          GOENV = "${tmpDir}/go/env";
+          GOCACHE = "${tmpDir}/go/cache";
+          GOMODCACHE = "${tmpDir}/go/pkg/mod";
+          GOTELEMETRYDIR = "${tmpDir}/go/telemetry";
+        };
+      });
     };
 }
